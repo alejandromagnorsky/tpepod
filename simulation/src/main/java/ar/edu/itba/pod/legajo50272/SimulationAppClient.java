563dbf1b0df100ea53fde8122fc272fef98438a1
alejandromagnorsky <alejandromagnorsky@d51c9b0d-e013-4688-e655-fb72c6f7dda0>
2011-11-04 22:24:57 +0000

    Mas de la simulacion

--- simulation/src/main/java/ar/edu/itba/pod/legajo50272/SimulationAppClient.java
+++ simulation/src/main/java/ar/edu/itba/pod/legajo50272/SimulationAppClient.java
@@ -2,50 +2,53 @@ package ar.edu.itba.pod.legajo50272;
 
 import org.joda.time.Duration;
 
-import ar.edu.itba.pod.agent.market.AgentState;
-import ar.edu.itba.pod.agent.runner.Agent;
-import ar.edu.itba.pod.agent.runner.Environment;
+import ar.edu.itba.pod.agent.market.Consumer;
+import ar.edu.itba.pod.agent.market.Market;
+import ar.edu.itba.pod.agent.market.Producer;
+import ar.edu.itba.pod.agent.market.Resource;
 import ar.edu.itba.pod.time.TimeMapper;
 import ar.edu.itba.pod.time.TimeMappers;
 
 public class SimulationAppClient {
 
-	private static String host = "localhost";
+	private static String host = "10.6.0.101";
 	private static int port = 1099;
-	private static String serverHost = "localhost";
+	private static String serverHost = "10.6.0.44";
 	private static int serverPort = 1100;
 	private static String id = host + port;
-	
-	public static void main(String[] args) {
-		TimeMapper timeMapper = TimeMappers.oneSecondEach(Duration.standardHours(6));
-		NodeImpl node = new NodeImpl(host, port, id, timeMapper);
-		node.startServices();
-		try {
-			node.getClusterAdministration().connectToGroup(serverHost, serverPort);
-			RemoteSimulation remoteSimulation = new RemoteSimulation(node);
-			for(int i = 0; i < 10; i++){
-				final String name = "Agent"+i;
-				remoteSimulation.add(new Agent(name){
-					@Override
-					public void execute(Environment env) {
-						System.out.println(super.name());
-						try {
-							Thread.sleep(1000);
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-					}
 
-					@Override
-					public AgentState state() {
-						return null;
-					}		
-				});
-			}			
-			remoteSimulation.start(Duration.standardMinutes(10));
-		} catch (Exception e) {
-			e.printStackTrace();
+	public static void main(String[] args) {
+		TimeMapper timeMapper = TimeMappers.oneSecondEach(Duration
+				.standardHours(6));
+		RemoteSimulation remoteSimulation = new RemoteSimulation(host, port,
+				id, serverHost, serverPort, timeMapper);
+
+		Resource gold = new Resource("Mineral", "Gold");
+		Resource copper = new Resource("Mineral", "Copper");
+		Resource steel = new Resource("Alloy", "Steel");
+
+		for (int i = 0; i < 1; i++) {
+			remoteSimulation.add(new Producer("steel mine" + i, steel, Duration
+					.standardDays(1), 5));
+			remoteSimulation.add(new Producer("copper mine" + i, copper,
+					Duration.standardDays(1), 10));
+			remoteSimulation.add(new Producer("gold mine" + i, gold, Duration
+					.standardDays(1), 1));
+
+			remoteSimulation.add(new Consumer("steel consumer" + i, steel,
+					Duration.standardDays(3), 2));
+			remoteSimulation.add(new Consumer("copper consumer" + i, copper,
+					Duration.standardHours(8), 2));
+			remoteSimulation.add(new Consumer("gold consumer" + i, gold,
+					Duration.standardDays(2), 4));
 		}
+
+		remoteSimulation.add(new Market("gold market", gold));
+		remoteSimulation.add(new Market("cooper market", copper));
+		remoteSimulation.add(new Market("steel market", steel));
+
+		remoteSimulation.start(Duration.standardMinutes(10));
+
 		System.out.println("Node started");
 	}
 }
